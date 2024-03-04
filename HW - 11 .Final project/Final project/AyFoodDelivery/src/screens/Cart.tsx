86879552a8f5d94dc74d548669d545e32c094182
AyKhanZ <zeynalovayxan70@gmail.com>
2024-03-04 18:12:50 +0400

    Cart products Done sukur

--- HW - 11 .Final project/Final project/AyFoodDelivery/src/screens/Cart.tsx
+++ HW - 11 .Final project/Final project/AyFoodDelivery/src/screens/Cart.tsx
@@ -1,24 +1,41 @@
 import { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import { useCart } from "../context/CartContext";
 import { Users } from "../data/Users";
 import ProductItem from "../components/ProductItem";
 
 const Cart = () => {
-  const [cartObj, setCartObj] = useState({});
-  const [products, setProducts] = useState([]);
+  const [ifPressed, setIfPressed] = useState(false);
+  // const [products, setProducts] = useState([]);
+  let cart = useCart();
+  const email = cart.cartObj.email;
+  console.log(email);
+
   // useEffect(() => {
-  //   let c = useCart();
-  //   // setCartObj(c);
-  //   console.log(c);
+  //   console.log(Users);
+  // }, [Users]);
+  const showProducts = (c: any, n: number) => {
+    return c.map((p: any, n: number) => <Text key={n}>{p.title}</Text>);
+  };
+  const showUserProducts = () => {
+    return Users.map((u: any, i: number) => {
+      if (u.email == email) {
+        console.log(u.cart);
+        return showProducts(u.cart, i);
+      }
+    });
+  };
+  // useEffect(() => {
+  //   showUserProducts();
   // }, [Users]);
   return (
     <View>
       <Text>Cart</Text>
 
-      {/* {products.map((product, i) => (
-        <Text key={i}>{product.title}</Text>
-      ))} */}
+      <Pressable onPress={() => setIfPressed(!ifPressed)}>
+        <Text>Click</Text>
+      </Pressable>
+      {ifPressed ? showUserProducts() : <></>}
     </View>
   );
 };
